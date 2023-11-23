594ee212da191df5b9207e557a08f72cf16d40d6
Hüseyin Yener <huseyin.yener@bestcloudfor.me>
2023-11-23 16:07:20 +0300

    new

---
+++ app/src/main/java/com/example/androidjenkinsapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.androidjenkinsapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.bestcloudfor.me");
+    }
+}
